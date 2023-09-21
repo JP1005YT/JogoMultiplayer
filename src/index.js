29b214b3b544ee1defc38282db5e1d18543d7544
JP1005YT <godlolpro32@gmail.com>
2023-09-21 19:21:55 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const { Socket } = require("socket.io")
-const {Pacotes} = require("../configs/Pacotes.js")
+const { Pacotes } = require("../configs/Pacotes.js")
 let P = new Pacotes()
 
-const {Server} = require("../configs/Server.js")
+const { Server } = require("../configs/Server.js")
 let S = new Server()
 
 S.start()
 
 let app = S.app
 let isLoged = {}
+let playersObj = [{}]
 
 S.io.on('connection', (socket) => {
 
-    socket.on('data',async (player)=>{
+    socket.on('data', async (player) => {
         let Exists
         for (const chave in isLoged) {
             if (isLoged.hasOwnProperty(chave)) {
@@ -21,36 +22,46 @@ S.io.on('connection', (socket) => {
                 }
             }
         }
-        if(!Exists){
+        if (!Exists) {
             isLoged[`${socket.id}`] = player
-            
+
         }
-        let puxar = await AdquirePlayerLogStats()
-        S.io.emit("updatePlayers", puxar)
+        playersObj = await AdquirePlayerLogStats()
+        S.io.emit("loadPlayers", playersObj)
     });
 
-    socket.on('disconnect',async () => {
+    socket.on("positionUpdate", async (data) => {
+        playersObj.forEach(player => {
+            if (player.name === data[2]) {
+                player.posX = data[0];
+                player.posY = data[1];
+            }
+        })
+        S.io.emit("updatePlayers", playersObj);
+    })
+
+    socket.on('disconnect', async () => {
         delete isLoged[socket.id]
-        let puxar = await AdquirePlayerLogStats()
-        S.io.emit("updatePlayers", puxar)
+        playersObj = await AdquirePlayerLogStats()
+        S.io.emit("updatePlayers", playersObj)
     })
 });
 
-async function AdquirePlayerLogStats(){
+async function AdquirePlayerLogStats() {
     let data = P.Buscar('./data/users.json')
     let nameslogged = []
     let dataofFinal = []
-    
+
     for (const chave in isLoged) {
         if (isLoged.hasOwnProperty(chave)) {
             nameslogged.push(isLoged[chave])
         }
     }
 
-    
+
     nameslogged.forEach(nome => {
         data.users.forEach(usuario => {
-            if(usuario.name === nome){
+            if (usuario.name === nome) {
                 dataofFinal.push(usuario)
             }
         })
@@ -59,36 +70,36 @@ async function AdquirePlayerLogStats(){
     return dataofFinal
 }
 
-app.get('/login', async (req,res) => {
+app.get('/login', async (req, res) => {
     let dados = JSON.parse(req.headers.data)
     let data = P.Buscar("./data/users.json")
     data.users.forEach(element => {
-        if(element.name === dados.user){
+        if (element.name === dados.user) {
             element.Token = P.uid.v4()
             res.send(element)
-            P.Guardar('./data/users.json',data)
+            P.Guardar('./data/users.json', data)
         }
     });
 })
-app.get('/play',async (req,res) => {
+app.get('/play', async (req, res) => {
     let data = P.Buscar("./data/users.json")
     let caminho
     data.users.forEach(element => {
-        if(element.Token === req.query.token){
-            caminho = P.path.join(__dirname, '../', 'pages' , 'game' , 'index.html')
+        if (element.Token === req.query.token) {
+            caminho = P.path.join(__dirname, '../', 'pages', 'game', 'index.html')
         }
     })
-    if(caminho){
+    if (caminho) {
         res.sendFile(caminho)
-    }else{
+    } else {
         res.send('broxo')
     }
 })
-app.get('/user',async(req,res) => {
+app.get('/user', async (req, res) => {
     let dados = JSON.parse(req.headers.data)
     let data = P.Buscar("./data/users.json")
     data.users.forEach(element => {
-        if(element.Token === dados.token){
+        if (element.Token === dados.token) {
             res.send(element)
         }
     });
