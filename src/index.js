e3151429159fe2093b8f128e8abf0217d921fa67
teemago <65630024+teemago@users.noreply.github.com>
2023-09-23 00:59:41 -0300

    finished basic

--- src/index.js
+++ src/index.js
@@ -1,80 +1,88 @@
-const { Socket } = require("socket.io")
-const { Pacotes } = require("../configs/Pacotes.js")
-let P = new Pacotes()
+const { Socket } = require("socket.io");
+const { Pacotes } = require("../configs/Pacotes.js");
+let P = new Pacotes();
 
-const { Server } = require("../configs/Server.js")
-let S = new Server()
+const { Server } = require("../configs/Server.js");
+let S = new Server();
 
-S.start()
+S.start();
 
-let app = S.app
-const Sio = S.io
-let LoggedPlayers = []
+let app = S.app;
+const Sio = S.io;
+let LoggedPlayers = [];
 
 Sio.on("connection", (socket) => {
-    socket.on("connected", async (token) => {
-        let logged = await FetchLoggedPlayers(token);
-        logged['socketID'] = socket.id
-        LoggedPlayers.push(logged);
-        Sio.emit("UpdatePlayers", LoggedPlayers);
-
+  socket.on("connected", async (token) => {
+    let logged = await FetchLoggedPlayers(token);
+    logged["socketID"] = socket.id;
+    LoggedPlayers.push(logged);
+    Sio.emit("UpdatePlayers", LoggedPlayers);
+  });
+  socket.on("disconnect", async () => {
+    LoggedPlayers.forEach((player, index) => {
+      if (player.socketID === socket.id) {
+        LoggedPlayers.splice(index, 1);
+      }
     });
-    socket.on("disconnect", async () => {
-        LoggedPlayers.forEach((player, index) => {
-            if (player.socketID === socket.id) {
-                LoggedPlayers.splice(index, 1)
-            }
-        })
-        Sio.emit("UpdatePlayers", LoggedPlayers); 
-    })
+    Sio.emit("UpdatePlayers", LoggedPlayers);
+  });
 
-})
+  socket.on("positionUpdate", (positionPlayer) => {
+    LoggedPlayers.forEach((player) => {
+      if (player.name === positionPlayer[2]) {
+        player.posX = positionPlayer[0];
+        player.posY = positionPlayer[1];
+      }
+    });
+    Sio.emit("UpdatePlayers", LoggedPlayers);
+  });
+});
 
 async function FetchLoggedPlayers(token) {
-    const data = await P.Buscar('./data/users.json')
+  const data = await P.Buscar("./data/users.json");
 
-    let Player
+  let Player;
 
-    data.forEach(user => {
-        if (user.Token === token) {
-            Player = user
-        }
-        return;
-    })
-    return Player;
+  data.forEach((user) => {
+    if (user.Token === token) {
+      Player = user;
+    }
+    return;
+  });
+  return Player;
 }
 
-app.get('/login', async (req, res) => {
-    let dados = JSON.parse(req.headers.data)
-    let data = P.Buscar("./data/users.json")
-    data.forEach(element => {
-        if (element.name === dados.user) {
-            element.Token = P.uid.v4()
-            res.send(element)
-            P.Guardar('./data/users.json', data)
-        }
-    });
-})
-app.get('/play', async (req, res) => {
-    let data = P.Buscar("./data/users.json")
-    let caminho
-    data.forEach(element => {
-        if (element.Token === req.query.token) {
-            caminho = P.path.join(__dirname, '../', 'pages', 'game', 'index.html')
-        }
-    })
-    if (caminho) {
-        res.sendFile(caminho)
-    } else {
-        res.send('broxo')
+app.get("/login", async (req, res) => {
+  let dados = JSON.parse(req.headers.data);
+  let data = P.Buscar("./data/users.json");
+  data.forEach((element) => {
+    if (element.name === dados.user) {
+      element.Token = P.uid.v4();
+      res.send(element);
+      P.Guardar("./data/users.json", data);
     }
-})
-app.get('/user', async (req, res) => {
-    let dados = JSON.parse(req.headers.data)
-    let data = P.Buscar("./data/users.json")
-    data.forEach(element => {
-        if (element.Token === dados.token) {
-            res.send(element)
-        }
-    });
-})
+  });
+});
+app.get("/play", async (req, res) => {
+  let data = P.Buscar("./data/users.json");
+  let caminho;
+  data.forEach((element) => {
+    if (element.Token === req.query.token) {
+      caminho = P.path.join(__dirname, "../", "pages", "game", "index.html");
+    }
+  });
+  if (caminho) {
+    res.sendFile(caminho);
+  } else {
+    res.send("broxo");
+  }
+});
+app.get("/user", async (req, res) => {
+  let dados = JSON.parse(req.headers.data);
+  let data = P.Buscar("./data/users.json");
+  data.forEach((element) => {
+    if (element.Token === dados.token) {
+      res.send(element);
+    }
+  });
+});
