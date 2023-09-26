00cbc7e772ceabc1e2dcbbc3db4b5e9401c33a33
Arhtur <nonakaodaarthur@gmail.com>
2023-09-26 14:26:23 -0300

    batata

--- configs/Library.js
+++ configs/Library.js
@@ -6,20 +6,18 @@ class Game {
         this.myGameArea = {
             canvas: document.createElement("canvas"),
             start: function () {
-                const WIDTH = 1000,
-                    HEIGHT = 600;
-                myGameArea.canvas.width = WIDTH;
-                this.canvas.height = HEIGHT;
-                this.context = this.canvas.getContext("2d");
-                container.appendChild(this.canvas);
-                this.interval = setInterval(updateGameArea, 7);
+                myGameArea.canvas.width = this.ScreenWidth;
+                myGameArea.canvas.height = this.ScreenHeight;
+                myGameArea.context = myGameArea.canvas.getContext("2d");
+                document.getElementById("container").appendChild(myGameArea.canvas);
+                myGameArea.interval = setInterval(updateGameArea, 7);
             },
             clear: function () {
-                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                myGameArea.context.clearRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
             },
         };
         this.CreateSquare = function Square(name, width, x, y, color, socketID) {
-            this.ObjElements = {
+            this.ObjElements[this.RandomID(4)] = {
                 "name": name,
                 "x": x,
                 "y": y,
@@ -32,42 +30,100 @@ class Game {
                 "socket": socketID,
                 "width": width,
                 "update": function () {
+                    ctx = myGameArea.context;
+                    ctx.font = "bold 15px Arial";
+                    ctx.fillStyle = this.color;
+                    ctx.fillRect(this.x, this.y, this.width, this.width);
+                    ctx.fillText(this.name, this.x, this.y - 7);
+                    if (this.isRight || this.isLeft || this.isBottom || this.isTop) {
+                        socket.emit("positionUpdate", [this.x, this.y, this.name]);
+                    }
+                },
+                "newPos": function () {
+                    // if (this.x + this.width >= collision.x &&
+                    //     this.x <= collision.x + collision.h &&
+                    //     this.y + this.width >= collision.y &&
+                    //     this.y <= collision.y + collision.v
+                    // ) {
+                    //     collision.verify(this.x, this.y, this.width);
+                    // }
 
+                    if (this.isRight) {
+                        this.x += this.speed;
+                    }
+                    if (this.isLeft) {
+                        this.x -= this.speed;
+                    }
+                    if (this.isTop) {
+                        this.y += this.speed;
+                    }
+                    if (this.isBottom) {
+                        this.y -= this.speed;
+                    }
                 }
             }
-            this.update = function () {
-                ctx = myGameArea.context;
-                ctx.font = "bold 15px Arial";
-                ctx.fillStyle = this.color;
-                ctx.fillRect(this.x, this.y, width, width);
-                ctx.fillText(this.name, this.x, this.y - 7);
-                if (this.isRight || this.isLeft || this.isBottom || this.isTop) {
-                    socket.emit("positionUpdate", [this.x, this.y, this.name]);
-                }
-            };
-            this.newPos = function () {
-                if (this.x + this.width >= collision.x &&
-                    this.x <= collision.x + collision.h &&
-                    this.y + this.width >= collision.y &&
-                    this.y <= collision.y + collision.v
-                ) {
-                    collision.verify(this.x, this.y, this.width);
-                }
+        };
 
 
-                if (this.isRight) {
-                    this.x += this.speed;
-                }
-                if (this.isLeft) {
-                    this.x -= this.speed;
-                }
-                if (this.isTop) {
-                    this.y += this.speed;
-                }
-                if (this.isBottom) {
-                    this.y -= this.speed;
-                }
-            };
+        // this.ObjectCollision = function ObjectCollision(x, y, horizontal, vertical) {
+        //     this.ObjElements[this.randomID(4)] = {
+        //         "x": x,
+        //         "y": y,
+        //         "h": horizontal,
+        //         "v": vertical,
+        //         "update": function () {
+        //             ctx = myGameArea.context;
+        //             ctx.fillStyle = "Green";
+        //             ctx.fillRect(this.x, this.y, this.h, this.v);
+        //         },
+
+
+        //         "verify": function (playerX, playerY, playerWidth) {
+
+        //             if (playerY + playerWidth <= this.y) {
+        //                 console.log("encima")
+        //                 for (const square in players) {
+        //                     if (players[square].socket === socket.id) {
+        //                         players[square].isTop = false;
+        //                     }
+        //                 }
+        //             } if (playerY >= this.y + this.v) {
+        //                 console.log("embaixo ")
+        //                 for (const square in players) {
+        //                     if (players[square].socket === socket.id) {
+        //                         players[square].isBottom = false;
+        //                     }
+        //                 }
+        //             }
+        //             if (playerX + playerWidth <= this.x) {
+        //                 console.log("esquerda")
+        //                 for (const square in players) {
+        //                     if (players[square].socket === socket.id) {
+        //                         players[square].isRight = false;
+        //                     }
+        //                 }
+        //             } if (playerX >= this.x + this.h) {
+        //                 console.log("direita")
+        //                 for (const square in players) {
+        //                     if (players[square].socket === socket.id) {
+        //                         players[square].isLeft = false;
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // };
+
+    };
+    RandomID(length) {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
         }
+        return result;
     }
-}
+};
